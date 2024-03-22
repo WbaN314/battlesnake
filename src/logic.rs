1e05c8295f7dab474432dc3a8428ccf8e9b87d4d
sascha <sascha.stoll@sap.com>
2024-03-22 16:14:36 +0100

    Added DEPTH

--- src/logic.rs
+++ src/logic.rs
@@ -207,7 +207,7 @@ mod hungry_simple_snake {
 mod simple_tree_search_snake {
     use super::*;
 
-    fn simulate_snakes_step(_turn: &i32, board: &Board, _you: &Battlesnake) -> Vec<Board> {
+    fn simulate_snakes_step(board: &Board) -> Vec<Board> {
         let mut new_boards = Vec::with_capacity(board.snakes.len().pow(4));
 
         let mut decisions = Directions::new(board.snakes.len());
@@ -243,148 +243,154 @@ mod simple_tree_search_snake {
         Battle(usize),
     }
 
-    fn evaluate_snakes_step(_turn: &i32, board: &mut Board, you: &Battlesnake) -> (Direction, i32) {
+    fn evaluate_snakes_step(board: &mut Board, you: &String) -> (Direction, i32) {
         // find own snake
-        let mut own_snake_index = 0;
+        let mut own_snake_index = None;
         for i in 0..board.snakes.len() {
-            if board.snakes[i].id == you.id {
-                own_snake_index = i;
+            if board.snakes[i].id == *you {
+                own_snake_index = Some(i);
                 break;
             }
         }
-        let own_snake = &board.snakes[own_snake_index];
 
-        // Find moved direction
-        let own_direction = match (
-            own_snake.head.x - own_snake.body[0].x,
-            own_snake.head.y - own_snake.body[0].y,
-        ) {
-            (1, 0) => Direction::Right,
-            (-1, 0) => Direction::Left,
-            (0, 1) => Direction::Up,
-            (0, -1) => Direction::Down,
-            _ => unreachable!(),
-        };
+        if let Some(own_snake_index) = own_snake_index {
+            let own_snake = &board.snakes[own_snake_index];
 
-        // check out of bounds of own snake
-        if own_snake.head.x < 0
-            || own_snake.head.x >= board.width
-            || own_snake.head.y < 0
-            || own_snake.head.y >= board.height as i32
-        {
-            debug!("{} -1", own_direction);
-            return (own_direction, -1);
-        };
+            // Find moved direction
+            let own_direction = match (
+                own_snake.head.x - own_snake.body[0].x,
+                own_snake.head.y - own_snake.body[0].y,
+            ) {
+                (1, 0) => Direction::Right,
+                (-1, 0) => Direction::Left,
+                (0, 1) => Direction::Up,
+                (0, -1) => Direction::Down,
+                _ => unreachable!(),
+            };
 
-        let mut snake_changes = vec![SnakeChange::None; board.snakes.len()];
-
-        // check for battles
-        for i in 0..board.snakes.len() {
-            for j in i + 1..board.snakes.len() {
-                let x1 = board.snakes[i].head.x;
-                let y1 = board.snakes[i].head.y;
-                let x2 = board.snakes[j].head.x;
-                let y2 = board.snakes[j].head.y;
-                if x1 == x2 && y1 == y2 {
-                    snake_changes[i] = SnakeChange::Battle(j);
-                    break;
-                }
-            }
-        }
-
-        // Resolve battles
-        let mut battle_results = snake_changes.clone();
-        for i in 0..snake_changes.len() {
-            match snake_changes[i] {
-                SnakeChange::Battle(j) => {
-                    if board.snakes[i].length < board.snakes[j].length {
-                        battle_results[i] = SnakeChange::Die;
-                    } else if board.snakes[i].length == board.snakes[j].length {
-                        battle_results[i] = SnakeChange::Die;
-                        battle_results[j] = SnakeChange::Die;
-                    } else {
-                        battle_results[j] = SnakeChange::Die;
-                    }
-                }
-                _ => (),
-            }
-        }
-        snake_changes = battle_results;
-
-        // Evaluate non battle deaths
-        for i in 0..board.snakes.len() {
-            let snake = &board.snakes[i];
-            if snake.head.x >= board.width
-                || snake.head.x < 0
-                || snake.head.y >= board.height as i32
-                || snake.head.y < 0
+            // check out of bounds of own snake
+            if own_snake.head.x < 0
+                || own_snake.head.x >= board.width
+                || own_snake.head.y < 0
+                || own_snake.head.y >= board.height as i32
             {
-                snake_changes[i] = SnakeChange::Die;
-                continue;
-            }
-            for other_snake in board.snakes.iter() {
-                for part in other_snake.body.iter() {
-                    if part.x == snake.head.x && part.y == snake.head.y {
-                        snake_changes[i] = SnakeChange::Die;
+                debug!("{} dead", own_direction);
+                return (own_direction, -10);
+            };
+
+            let mut snake_changes = vec![SnakeChange::None; board.snakes.len()];
+
+            // check for battles
+            for i in 0..board.snakes.len() {
+                for j in i + 1..board.snakes.len() {
+                    let x1 = board.snakes[i].head.x;
+                    let y1 = board.snakes[i].head.y;
+                    let x2 = board.snakes[j].head.x;
+                    let y2 = board.snakes[j].head.y;
+                    if x1 == x2 && y1 == y2 {
+                        snake_changes[i] = SnakeChange::Battle(j);
                         break;
                     }
                 }
             }
-        }
 
-        // Evaluate food
-        for i in 0..board.snakes.len() {
-            let snake = &board.snakes[i];
-            for food in board.food.iter() {
-                if food.x == snake.head.x
-                    && food.y == snake.head.y
-                    && snake_changes[i] != SnakeChange::Die
+            // Resolve battles
+            let mut battle_results = snake_changes.clone();
+            for i in 0..snake_changes.len() {
+                match snake_changes[i] {
+                    SnakeChange::Battle(j) => {
+                        if board.snakes[i].length < board.snakes[j].length {
+                            battle_results[i] = SnakeChange::Die;
+                        } else if board.snakes[i].length == board.snakes[j].length {
+                            battle_results[i] = SnakeChange::Die;
+                            battle_results[j] = SnakeChange::Die;
+                        } else {
+                            battle_results[j] = SnakeChange::Die;
+                        }
+                    }
+                    _ => (),
+                }
+            }
+            snake_changes = battle_results;
+
+            // Evaluate non battle deaths
+            for i in 0..board.snakes.len() {
+                let snake = &board.snakes[i];
+                if snake.head.x >= board.width
+                    || snake.head.x < 0
+                    || snake.head.y >= board.height as i32
+                    || snake.head.y < 0
                 {
-                    snake_changes[i] = SnakeChange::Grow;
+                    snake_changes[i] = SnakeChange::Die;
+                    continue;
+                }
+                for other_snake in board.snakes.iter() {
+                    for part in other_snake.body.iter() {
+                        if part.x == snake.head.x && part.y == snake.head.y {
+                            snake_changes[i] = SnakeChange::Die;
+                            break;
+                        }
+                    }
                 }
             }
-        }
 
-        // Evaluate final snake changes
-        let mut new_snakes = Vec::new();
-        for i in 0..board.snakes.len() {
-            match snake_changes[i] {
+            // Evaluate food
+            for i in 0..board.snakes.len() {
+                let snake = &board.snakes[i];
+                for food in board.food.iter() {
+                    if food.x == snake.head.x
+                        && food.y == snake.head.y
+                        && snake_changes[i] != SnakeChange::Die
+                    {
+                        snake_changes[i] = SnakeChange::Grow;
+                    }
+                }
+            }
+
+            // Evaluate final snake changes
+            let mut new_snakes = Vec::new();
+            for i in 0..board.snakes.len() {
+                match snake_changes[i] {
+                    SnakeChange::Grow => {
+                        let mut new_body = board.snakes[i].body.clone();
+                        new_body.push(board.snakes[i].head);
+                        new_body.rotate_right(1);
+                        board.snakes[i].body = new_body;
+                        new_snakes.push(board.snakes[i].clone());
+                    }
+                    SnakeChange::Battle(_) | SnakeChange::None => {
+                        let l = board.snakes[i].body.len() - 1;
+                        board.snakes[i].body[l] = board.snakes[i].head;
+                        board.snakes[i].body.rotate_right(1);
+                        new_snakes.push(board.snakes[i].clone())
+                    }
+                    SnakeChange::Die => (),
+                }
+            }
+            board.snakes = new_snakes;
+
+            // evaluate board
+            match snake_changes[own_snake_index] {
+                SnakeChange::Die => {
+                    debug!("{} dead", own_direction);
+                    (own_direction, -10)
+                }
+                SnakeChange::Battle(_) => {
+                    debug!("{} 3", own_direction);
+                    (own_direction, 3)
+                }
                 SnakeChange::Grow => {
-                    let mut new_body = board.snakes[i].body.clone();
-                    new_body.push(board.snakes[i].head);
-                    new_body.rotate_right(1);
-                    board.snakes[i].body = new_body;
-                    new_snakes.push(board.snakes[i].clone());
+                    debug!("{} 2", own_direction);
+                    (own_direction, 2)
                 }
-                SnakeChange::Battle(_) | SnakeChange::None => {
-                    let l = board.snakes[i].body.len() - 1;
-                    board.snakes[i].body[l] = board.snakes[i].head;
-                    board.snakes[i].body.rotate_right(1);
-                    new_snakes.push(board.snakes[i].clone())
+                SnakeChange::None => {
+                    debug!("{} 1", own_direction);
+                    (own_direction, 1)
                 }
-                SnakeChange::Die => (),
             }
-        }
-        board.snakes = new_snakes;
-
-        // evaluate board
-        match snake_changes[own_snake_index] {
-            SnakeChange::Die => {
-                debug!("{} -1", own_direction);
-                (own_direction, -1)
-            }
-            SnakeChange::Battle(_) => {
-                debug!("{} 3", own_direction);
-                (own_direction, 3)
-            }
-            SnakeChange::Grow => {
-                debug!("{} 2", own_direction);
-                (own_direction, 2)
-            }
-            SnakeChange::None => {
-                debug!("{} 1", own_direction);
-                (own_direction, 1)
-            }
+        } else {
+            // no own snake
+            (Direction::Up, 0)
         }
     }
 
@@ -435,16 +441,23 @@ mod simple_tree_search_snake {
     }
 
     impl Brain for SimpleTreeSearchSnake {
-        fn logic(&self, _game: &Game, turn: &i32, board: &Board, you: &Battlesnake) -> Direction {
-            let mut new_boards = simulate_snakes_step(turn, board, you);
+        fn logic(&self, _game: &Game, _turn: &i32, board: &Board, you: &Battlesnake) -> Direction {
+            let (mut new_boards, mut move_scores) = step(board, &you.id);
 
-            let mut move_scores = [0; 4];
-            for board in new_boards.iter_mut() {
-                match evaluate_snakes_step(turn, board, you) {
-                    (Direction::Up, score) => move_scores[0] += score,
-                    (Direction::Down, score) => move_scores[1] += score,
-                    (Direction::Left, score) => move_scores[2] += score,
-                    (Direction::Right, score) => move_scores[3] += score,
+            if let Ok(value) = env::var("DEPTH") {
+                let value = value.parse().unwrap();
+                for _ in 0..value {
+                    let mut tmp_boards = Vec::with_capacity(
+                        (4_i32.pow(board.snakes.len() as u32) * new_boards.len() as i32) as usize,
+                    );
+                    for board in new_boards.iter() {
+                        let (mut b, s) = step(board, &you.id);
+                        for i in 0..4 {
+                            move_scores[i] = move_scores[i].saturating_add(s[i]);
+                        }
+                        tmp_boards.append(&mut b);
+                    }
+                    new_boards = tmp_boards;
                 }
             }
 
@@ -465,6 +478,29 @@ mod simple_tree_search_snake {
             }
             best_move
         }
+    }
+
+    fn step(board: &Board, id: &String) -> (Vec<Board>, [i32; 4]) {
+        let mut new_boards = simulate_snakes_step(board);
+
+        let mut move_scores = [0; 4];
+        for board in new_boards.iter_mut() {
+            match evaluate_snakes_step(board, id) {
+                (Direction::Up, score) => {
+                    move_scores[0] = (move_scores[0] as i32).saturating_add(score)
+                }
+                (Direction::Down, score) => {
+                    move_scores[1] = (move_scores[1] as i32).saturating_add(score)
+                }
+                (Direction::Left, score) => {
+                    move_scores[2] = (move_scores[2] as i32).saturating_add(score)
+                }
+                (Direction::Right, score) => {
+                    move_scores[3] = (move_scores[3] as i32).saturating_add(score)
+                }
+            };
+        }
+        (new_boards, move_scores)
     }
 
     #[cfg(test)]
